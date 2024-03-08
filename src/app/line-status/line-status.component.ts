3645abc2a4fcbb3d50ec555cf566fb09ae3c2722
adrigm <adrigm.admin@gmail.com>
2024-03-08 16:53:20 +0100

    updae

--- src/app/line-status/line-status.component.ts
+++ src/app/line-status/line-status.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import {CdkDrag, CdkDragEnd, CdkDragMove, CdkDragStart} from '@angular/cdk/drag-drop';
 import { MapObjectComponent } from './components/map-object/map-object.component';
 
@@ -37,9 +37,10 @@ interface MapObject {
   styleUrl: './line-status.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LineStatusComponent {
+export class LineStatusComponent implements AfterViewInit {
   @ViewChild('zoomContainer') zoomContainer: ElementRef;
   @ViewChild('visor') visor: ElementRef;
+  @ViewChild('map') map: ElementRef;
 
   mapObjects: MapObject[] = [
     {
@@ -84,83 +85,123 @@ export class LineStatusComponent {
       text: '3',
       fontSize: 16,
       fontColor: 'white'
+    },
+    {
+      id: '3',
+      position: { x: 1900, y: 1900 },
+      adjustPosition: { x: 2000, y: 2000 },
+      size: { width: 100, height: 100 },
+      backgroundColor: 'blue',
+      borderColor: 'black',
+      borderWidth: 2,
+      borderRadius: 10,
+      zIndex: 1,
+      text: '3',
+      fontSize: 16,
+      fontColor: 'white'
     }
   ]
 
   zoomLevel = 1;
-  zoomOrigin = { x: 0, y: 0 };
   canvasSize = { width: 2000, height: 2000 };
 
   position = { x: 0, y: 0 };
   adjustedPosition = { x: 0, y: 0 };
 
-  adjustZoom(event: any) {
-    const delta = event.deltaY;
-
-    // max zoom level 4 min zoom level 0.5
-    if (delta > 0 && this.zoomLevel <= 0.5) {
-      return;
-    }
-
-    if (delta < 0 && this.zoomLevel >= 4) {
-      return;
-    }
-
-    this.zoomLevel = delta > 0 ? this.zoomLevel - 0.1 : this.zoomLevel + 0.1;
-
-    // Ajusta el origen del zoom basándote en la posición del ratón
-    this.zoomOrigin.x = event.offsetX;
-    this.zoomOrigin.y = event.offsetY;
-    console.log(this.zoomOrigin);
+  ngAfterViewInit() {
+    this.checkLimits();
   }
+
+  adjustZoom(event: any) {
+    console.log(event);
+    const element = this.zoomContainer.nativeElement;
+    const rect = element.getBoundingClientRect();
+    const prevZoomLevel = this.zoomLevel;
+
+    const mouseX = event.clientX - rect.left; // Posición X del ratón relativa al elemento
+    const mouseY = event.clientY - rect.top;  // Posición Y del ratón relativa al elemento
+
+    const delta = event.deltaY < 0 ? 1 : -1;  // Determinar dirección del zoom
+    const zoomIntensity = 0.1;
+    this.zoomLevel += delta * zoomIntensity;
+
+
+
+    // Limitar el nivel de zoom entre 0.5 y 4 (o cualquier rango deseado)
+    this.zoomLevel = Math.min(Math.max(this.zoomLevel, 0.5), 4);
+
+    // si el zoom no cambia, no hagas nada más
+    if (prevZoomLevel === this.zoomLevel) {
+      return;
+    }
+
+
+
+
+    // Cálculo de las nuevas posiciones basado en el punto de zoom
+    // Estos cálculos intentan mantener el punto bajo el cursor del ratón estático relativo al documento al aplicar el zoom
+    const newLeft = mouseX * (1 - this.zoomLevel / prevZoomLevel) + rect.left;
+    const newTop = mouseY * (1 - this.zoomLevel / prevZoomLevel) + rect.top;
+
+    // Aplicar el nuevo zoom y las posiciones al elemento
+    element.style.transform = `translate(${newLeft}px, ${newTop}px) scale(${this.zoomLevel})`;
+
+  }
+
+
+
 
   moveObject(event: any) {
     console.log(event);
   }
 
   onDrag(event: CdkDragMove<any>) {
-    // Calcula la distancia movida ajustada por el nivel de zoom
-    const adjustedX = event.distance.x / this.zoomLevel;
-    const adjustedY = event.distance.y / this.zoomLevel;
-
     // Actualiza la posición ajustada basándote en la posición inicial más la distancia movida
-    this.adjustedPosition.x = this.position.x + adjustedX;
-    this.adjustedPosition.y = this.position.y + adjustedY;
+    this.adjustedPosition.x = this.position.x + event.distance.x;
+    this.adjustedPosition.y = this.position.y + event.distance.y;
 
     // Aplica la transformación al elemento basándote en la posición ajustada acumulada
     const element = event.source.element.nativeElement;
-    this.zoomContainer.nativeElement.style.left = `${event.source.getFreeDragPosition().x}px`;
-    this.zoomContainer.nativeElement.style.top = `${event.source.getFreeDragPosition().y}px`;
-    element.style.transform = `translate(${0}px, ${0}px)`;
 
-    console.log(this.visor.nativeElement.offsetWidth / 2);
+    this.checkLimits();
 
-    // La parte izquierda del zoomContainer no puede ser mayor al centro del visor y la parte superior no puede ser mayor a la mitad del visor. Usa
-    if (event.source.getFreeDragPosition().x > this.visor.nativeElement.offsetWidth / 2) {
-      this.zoomContainer.nativeElement.style.left = `${this.visor.nativeElement.offsetWidth / 2}px`;
-    }
-    if (event.source.getFreeDragPosition().y > this.visor.nativeElement.offsetHeight / 2) {
-      this.zoomContainer.nativeElement.style.top = `${this.visor.nativeElement.offsetHeight / 2}px`;
-    }
-
-    // if (event.source.getFreeDragPosition().x > 0) {
-    //   this.zoomContainer.nativeElement.style.left = `${0}px`;
-    // }
-    // if (event.source.getFreeDragPosition().y > 0) {
-    //   this.zoomContainer.nativeElement.style.top = `${0}px`;
-    // }
-
-    // if (event.source.getFreeDragPosition().x < this.visor.nativeElement.offsetWidth - this.zoomContainer.nativeElement.offsetWidth) {
-    //   this.zoomContainer.nativeElement.style.left = `${this.visor.nativeElement.offsetWidth - this.zoomContainer.nativeElement.offsetWidth}px`;
-    // }
-    // if (event.source.getFreeDragPosition().y < this.visor.nativeElement.offsetHeight - this.zoomContainer.nativeElement.offsetHeight) {
-    //   this.zoomContainer.nativeElement.style.top = `${this.visor.nativeElement.offsetHeight - this.zoomContainer.nativeElement.offsetHeight}px`;
-    // }
 
 
   }
 
+  checkLimits() {
+    // Si el ancho del mapa es menor que el ancho del visor, no permitas que el mapa se mueva horizontalmente lo mismo con el eje Y y centra el mapa
+    if (this.canvasSize.width < this.visor.nativeElement.clientWidth) {
+      console.log('si');
+      this.adjustedPosition.x = (this.visor.nativeElement.clientWidth - this.canvasSize.width) / 2;
+    } else {
+      if (this.adjustedPosition.x > 0) {
+        this.adjustedPosition.x = 0;
+      }
+
+      if (this.adjustedPosition.x < -this.canvasSize.width + this.visor.nativeElement.clientWidth) {
+        this.adjustedPosition.x = -this.canvasSize.width + this.visor.nativeElement.clientWidth;
+      }
+    }
+
+    if (this.canvasSize.height < this.visor.nativeElement.clientHeight) {
+      this.adjustedPosition.y = (this.visor.nativeElement.clientHeight - this.canvasSize.height) / 2;
+    } else {
+      if (this.adjustedPosition.y > 0) {
+        this.adjustedPosition.y = 0;
+      }
+
+      if (this.adjustedPosition.y < -this.canvasSize.height + this.visor.nativeElement.clientHeight) {
+        this.adjustedPosition.y = -this.canvasSize.height + this.visor.nativeElement.clientHeight;
+      }
+    }
+
+
+    this.map.nativeElement.style.transform = `translate(${this.adjustedPosition.x}px, ${this.adjustedPosition.y}px)`;
+  }
+
   onDragEnd(event: CdkDragEnd) {
+
   }
 
   onDragStart(event: CdkDragStart) {
