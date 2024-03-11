1d2bfbc02511ffe6476d9cd1234ebe49a6fc8912
Adrián <adrigm.admin@gmail.com>
2024-03-11 11:18:21 +0100

    test

--- src/app/line-status/line-status.component.ts
+++ src/app/line-status/line-status.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import {CdkDrag, CdkDragEnd, CdkDragMove, CdkDragStart} from '@angular/cdk/drag-drop';
 import { MapObjectComponent } from './components/map-object/map-object.component';
+import Panzoom from '@panzoom/panzoom'
 
 interface Point {
   x: number;
@@ -31,14 +32,14 @@ interface MapObject {
   imports: [
     CommonModule,
     CdkDrag,
-    MapObjectComponent
+    MapObjectComponent,
   ],
   templateUrl: './line-status.component.html',
   styleUrl: './line-status.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LineStatusComponent {
-  @ViewChild('zoomContainer') zoomContainer: ElementRef;
+export class LineStatusComponent implements AfterViewInit {
+  @ViewChild('map') map: ElementRef;
   @ViewChild('visor') visor: ElementRef;
 
   mapObjects: MapObject[] = [
@@ -47,7 +48,7 @@ export class LineStatusComponent {
       position: { x: 100, y: 100 },
       adjustPosition: { x: 100, y: 100 },
       size: { width: 100, height: 100 },
-      backgroundColor: 'red',
+      backgroundColor: 'pink',
       borderColor: 'black',
       borderWidth: 2,
       borderRadius: 10,
@@ -88,86 +89,36 @@ export class LineStatusComponent {
   ]
 
   zoomLevel = 1;
-  zoomOrigin = { x: 0, y: 0 };
+  zoomOrigin: Point = { x: 0, y: 0 };
   canvasSize = { width: 2000, height: 2000 };
 
   position = { x: 0, y: 0 };
   adjustedPosition = { x: 0, y: 0 };
 
-  adjustZoom(event: any) {
-    const delta = event.deltaY;
+  ngAfterViewInit(): void {
+    const elem = document.getElementById('map')
+    console.log(elem);
+    const panzoom = Panzoom(elem, {
+      maxScale: 5,
+      minScale: 0.5,
+      canvas: true,
+      excludeClass: 'ls-map-object',
+      contain: 'outside',
+    });
+    console.log(panzoom);
+    panzoom.pan(0, 0)
+    panzoom.zoom(1, { animate: true })
 
-    // max zoom level 4 min zoom level 0.5
-    if (delta > 0 && this.zoomLevel <= 0.5) {
-      return;
-    }
 
-    if (delta < 0 && this.zoomLevel >= 4) {
-      return;
-    }
-
-    this.zoomLevel = delta > 0 ? this.zoomLevel - 0.1 : this.zoomLevel + 0.1;
-
-    // Ajusta el origen del zoom basándote en la posición del ratón
-    this.zoomOrigin.x = event.offsetX;
-    this.zoomOrigin.y = event.offsetY;
-    console.log(this.zoomOrigin);
+    // Panning and pinch zooming are bound automatically (unless disablePan is true).
+    // There are several available methods for zooming
+    // that can be bound on button clicks or mousewheel.
+    elem.parentElement.addEventListener('wheel', (e) => {
+      const zoom = panzoom.zoomWithWheel(e);
+      this.zoomLevel = zoom.scale;
+    });
   }
 
-  moveObject(event: any) {
-    console.log(event);
-  }
-
-  onDrag(event: CdkDragMove<any>) {
-    // Calcula la distancia movida ajustada por el nivel de zoom
-    const adjustedX = event.distance.x / this.zoomLevel;
-    const adjustedY = event.distance.y / this.zoomLevel;
-
-    // Actualiza la posición ajustada basándote en la posición inicial más la distancia movida
-    this.adjustedPosition.x = this.position.x + adjustedX;
-    this.adjustedPosition.y = this.position.y + adjustedY;
-
-    // Aplica la transformación al elemento basándote en la posición ajustada acumulada
-    const element = event.source.element.nativeElement;
-    this.zoomContainer.nativeElement.style.left = `${event.source.getFreeDragPosition().x}px`;
-    this.zoomContainer.nativeElement.style.top = `${event.source.getFreeDragPosition().y}px`;
-    element.style.transform = `translate(${0}px, ${0}px)`;
-
-    console.log(this.visor.nativeElement.offsetWidth / 2);
-
-    // La parte izquierda del zoomContainer no puede ser mayor al centro del visor y la parte superior no puede ser mayor a la mitad del visor. Usa
-    if (event.source.getFreeDragPosition().x > this.visor.nativeElement.offsetWidth / 2) {
-      this.zoomContainer.nativeElement.style.left = `${this.visor.nativeElement.offsetWidth / 2}px`;
-    }
-    if (event.source.getFreeDragPosition().y > this.visor.nativeElement.offsetHeight / 2) {
-      this.zoomContainer.nativeElement.style.top = `${this.visor.nativeElement.offsetHeight / 2}px`;
-    }
-
-    // if (event.source.getFreeDragPosition().x > 0) {
-    //   this.zoomContainer.nativeElement.style.left = `${0}px`;
-    // }
-    // if (event.source.getFreeDragPosition().y > 0) {
-    //   this.zoomContainer.nativeElement.style.top = `${0}px`;
-    // }
-
-    // if (event.source.getFreeDragPosition().x < this.visor.nativeElement.offsetWidth - this.zoomContainer.nativeElement.offsetWidth) {
-    //   this.zoomContainer.nativeElement.style.left = `${this.visor.nativeElement.offsetWidth - this.zoomContainer.nativeElement.offsetWidth}px`;
-    // }
-    // if (event.source.getFreeDragPosition().y < this.visor.nativeElement.offsetHeight - this.zoomContainer.nativeElement.offsetHeight) {
-    //   this.zoomContainer.nativeElement.style.top = `${this.visor.nativeElement.offsetHeight - this.zoomContainer.nativeElement.offsetHeight}px`;
-    // }
-
-
-  }
-
-  onDragEnd(event: CdkDragEnd) {
-  }
-
-  onDragStart(event: CdkDragStart) {
-    // Cuando el arrastre comienza, guarda la posición actual como la posición inicial
-    this.position.x = this.adjustedPosition.x;
-    this.position.y = this.adjustedPosition.y;
-  }
 
   onDragMapObject(event: CdkDragMove<any>, mapObject: MapObject) {
     // Calcula la distancia movida ajustada por el nivel de zoom
